09c0f15944189ff9f36dfe7f2965f95937bdfbf4
URK21CS3016 <109951881+URK21CS3016@users.noreply.github.com>
2022-10-09 22:06:54 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,20 +1,22 @@
 from typing import List
-def partition(array, low, high):
-    pivot = array[high]
-    i = low - 1
-    for j in range(low, high):
-        if array[j] <= pivot:
-            i = i + 1
-            (array[i], array[j]) = (array[j], array[i])
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
-    return i + 1
-def quick_sort(data, low, high) -> List[int]:
-    if low < high:
-        pi = partition(array, low, high)
-        quick_sort(array, low, pi - 1)
-        quick_sort(array, pi + 1, high)
-    return array
 
+def sorting(data,low,high):
+    # Write code here
+    i=low-1
+    pivot = data[high]
+    for j in range(low,high):
+        if data[j] <= pivot:
+            i+=1
+            data[i],data[j] = data[j],data[i]
+    data[i+1],data[high] = data[high],data[i+1]
+    return i+1
+
+def quick_sort(data, low, high) -> List[int]:
+    if low<high:
+        pi =sorting(data,low,high)
+        quick_sort(data,low,pi-1)
+        quick_sort(data,pi+1,high)
+        return data
 
 input_data = input()
 data = []
